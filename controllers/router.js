7222ef4295c4363dc9da8bc1a45c9c7a5b90ea7d
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-02-16 22:16:18 -0500

    make it better

--- controllers/router.js
+++ controllers/router.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router();
 
+
+router.get('/about', (req, res) => {
+    res.render('about.ejs')
+})
+
+router.get('/resume', (req, res) => {
+    res.render('resume.ejs')
+})
+
 router.get('/projects', (req, res) => {
     res.render('projects.ejs')
 })
+
+
 
 module.exports = router;
